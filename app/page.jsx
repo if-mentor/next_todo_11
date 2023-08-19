7c273be93bc1466f2d5646a0cfdd9817580c3589
yumi_23817 <candyclover.12815@gmail.com>
2023-08-19 21:33:13 +0900

    feature/todoEdit 作成

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,18 @@
 'use client'
 
 import { Button } from "@chakra-ui/react";
+import Link from "next/link";
 
 const Home = () => {
   return (
     <div>
       Hello, Team Development #11
+      <br />
+      <Link href="/edit">edit</Link>
+      <br />
+      <Link href="/create">create</Link>
+      <br />
+      <Button colorScheme='blue'>Button</Button>
     </div>
   )
 }
