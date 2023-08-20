e1643b8555132f599bee59e35b01956172eeaaad
shtk0119 <shtk0119@icloud.com>
2023-08-20 21:48:47 +0900

    firebase setup

---
+++ libs/Firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCeFsjinYftcCg5N55AN7dSeLmollcljQY",
+  authDomain: "next-todo-11.firebaseapp.com",
+  projectId: "next-todo-11",
+  storageBucket: "next-todo-11.appspot.com",
+  messagingSenderId: "1098935854485",
+  appId: "1:1098935854485:web:27f104fac70e671658429c"
+};
+
+const app = initializeApp(firebaseConfig);
