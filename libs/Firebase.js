bedeae9a202c191fe9b19b3b02bbf908ac7e2cef
shtk0119 <shtk0119@icloud.com>
2023-08-20 21:56:28 +0900

    Firebase Authenication & Firestore setup

--- libs/Firebase.js
+++ libs/Firebase.js
@@ -1,4 +1,6 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCeFsjinYftcCg5N55AN7dSeLmollcljQY",
@@ -10,3 +12,5 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
