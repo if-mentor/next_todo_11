7ce74a50f9956c3f5f255505bd1f808871ee8c49
コモディ <nobu54459@gmail.com>
2023-08-30 11:07:32 +0900

    修正しました。ご確認をお願いします

--- app/show/page.jsx
+++ app/show/page.jsx
@@ -2,26 +2,13 @@
 
 import { EditIcon } from '@chakra-ui/icons'
 
-import {
-  Box,
-  Button,
-  Flex,
-  Text,
-  Heading,
-  Spacer,
-  } from '@chakra-ui/react'
+import {Box,Button,Flex,Text,Heading,Spacer,} from '@chakra-ui/react'
 
 const Show = () => {
   return (
     <>
-      <Heading
-        bg="#68D391"
-        h="50px"
-        mb={5}
-        pl="50px"
-        pr='30px'
-        alignItems="center"
-      >
+      {/* ヘッダー */}
+      <Heading bg="#68D391" h="50px" mb={5} pl="50px" pr='30px' alignItems="center">
         <Flex>
          <Text>
           TODO
@@ -32,27 +19,21 @@ const Show = () => {
          </Button>
         </Flex>
       </Heading>
+
+      {/* コンテンツ */}
       <Box m='0 auto' maxW='1080px'>{/* コンテンツ部分の最大幅と横の余白 */}
+      
+        {/* CommentボタンとBackボタン */}
         <Flex mb='20px' justify="end">
-          <Button
-          mr='20px'
-          w='90px'
-          bgColor="green.700"
-          rounded="full"
-          color='white'
-          textAlign="center"
-          border='1px' borderColor='black'>
+          <Button mr='20px' w='90px' bgColor="green.700" rounded="full" color='white' textAlign="center" border='1px' borderColor='black'>
             Comment
           </Button>
-          <Button
-          w='90px'
-          bgColor="green.300"
-          rounded="full"
-          textAlign="center"
-          border='1px' borderColor='black'>
+          <Button w='90px' bgColor="green.300" rounded="full" textAlign="center" border='1px' borderColor='black'>
             Back
           </Button>
         </Flex>
+
+        {/* Todoリスト部分 */}
         <Flex>
           <Box w='55%' border='1px' borderColor='gray' p={2} mr='20px' borderRadius='10px' >
             <Box bg="#68D391">
@@ -66,11 +47,7 @@ const Show = () => {
               ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。
             </Text>
             <Flex mb='20px'>
-              <Button w='25%' mr='30px'
-              bgColor="green.300"
-              rounded="full"
-              textAlign="center"
-              border='1px' borderColor='black'>
+              <Button w='25%' mr='30px' bgColor="green.300" rounded="full" textAlign="center" border='1px' borderColor='black'>
                 Edit
                 <EditIcon ml='2'/>
               </Button>
@@ -84,6 +61,8 @@ const Show = () => {
               </Box>
             </Flex>
           </Box> 
+
+          {/* コメント部分 */}
           <Box mb='20px' border='1px' borderColor='gray' h='120px' w='45%'borderRadius='5px'>
             <Flex bgColor="green.600" color='white' px={3}>
               <Text>ジョン</Text>
