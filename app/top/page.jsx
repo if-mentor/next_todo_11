5100dd46b2940afac60ad5001634c86ce995d747
yumi_23817 <candyclover.12815@gmail.com>
2023-09-14 22:53:02 +0900

    feature/Top 機能実装

--- app/top/page.jsx
+++ app/top/page.jsx
@@ -1,53 +1,66 @@
 "use client";
 
 import { AddIcon, DeleteIcon, EditIcon, SearchIcon } from "@chakra-ui/icons";
-import { Heading, FormControl, FormLabel, Table, Thead, Tbody, Tr, Th, Td, TableContainer, Box, HStack, Button, Select, Input, InputRightElement, InputGroup, IconButton, Spacer } from "@chakra-ui/react";
-import Link from "next/link";
+import { FormControl, FormLabel, Table, Thead, Tbody, Tr, Th, Td, TableContainer, Box, HStack, Button, Select, Input, InputRightElement, InputGroup, IconButton, Spacer } from "@chakra-ui/react";
 import { useRouter } from "next/navigation";
-import { doc, getDoc } from "firebase/firestore"
+import { collection, deleteDoc, doc, getDocs, orderBy, query } from "firebase/firestore"
 import { useEffect, useState } from "react";
+import db from "../../firebase";
+import { format } from 'date-fns'
+import Link from "next/link";
 
 const Top = () => {
 
-  // const [todo, setTodo] = useState({
-  //   id: "",
-  //   text
-  // })
-
-  //const docRef = doc(db, )
-
-  //画面遷移のためのuseRouter
-  const router = useRouter();
+  //状態
+   const [todos, setTodos] = useState([])
+  //画面遷移用
+   const router = useRouter();
 
   //firebaseからデータを取得する
-  useEffect
+  useEffect(() => {
+    const todoData = collection(db, "posts");
+    //Updateを基準に降順で取得
+    const q = query(todoData, orderBy("Update", "desc"));
+    getDocs(q).then((snapShot) => {
+      const getTodoData = snapShot.docs.map((doc) => {
+        return { 
+          Create: format(doc.data().Create.toDate(), 'yyyy-MM-dd HH:mm'),
+          Detail: doc.data().Detail, 
+          Id: doc.data().Id, 
+          Priority: doc.data().Priority, 
+          Status: doc.data().Status, 
+          Task: doc.data().Task, 
+          Update: format(doc.data().Update.toDate(), 'yyyy-MM-dd HH:mm')
+        }
+      })
+      setTodos(getTodoData)
+      // console.log(todos)
+    })
+  }, [])
 
-  //Editページに遷移する関数
-  const linkToEdit = () => {
-    //firebaseからデータを取得
+  //Createページに遷移する関数
+  const linkToCreate = () => {
     //useRouterを使用した動的なページネーションの設定
-    //router.push(/edit/[id]/${id})
+    router.push("/create")
   }
 
-  //Showページに遷移する関数
-  const linkToShow = () => {
+  //Editページに遷移する関数
+  const linkToEdit = (Id) => {
     //useRouterを使用した動的なページネーションの設定
-    //router.push(/show/[id]/${id})
+    router.push(`/edit/${Id}`)
   }
 
   //Deleteボタン押下時に動く関数
-  const DeleteTodo = () => {
-
+   const DeleteTodo = (Id) => {
+    //firebaseの中のデータを削除する（バック側）
+    deleteDoc(doc(db,"posts", Id))
+    //表示するための処理（フロント側）
+    const deleteTodo = todos.filter((todo) => todo.Id !== Id)
+    setTodos(deleteTodo);
   }
 
   return (
     <>
-    {/* header */}
-      <Heading bg="#68D391" h="50px" mb={5} pl="50px" display="flex" alignItems="center">
-        TODO
-      </Heading>
-    {/* header */}
-
     {/* 中身 */}
       <Box px={20} py={6}>
         <HStack mb={4}>
@@ -102,13 +115,11 @@ const Top = () => {
           <Spacer />
 
           {/* createボタン */}
-          <Link href="/create">
             <Box>
-              <IconButton icon={<AddIcon />} colorScheme="teal" rounded="full" mr={2}>
+              <IconButton icon={<AddIcon />} colorScheme="teal" rounded="full" mr={2} onClick={linkToCreate}>
                 Task作成
               </IconButton>
             </Box>
-          </Link>
           {/* createボタン */}
 
         </HStack>
@@ -130,11 +141,17 @@ const Top = () => {
               {/* Todoリストのタイトル */}
 
             </Thead>
+
+            {/* Todoリスト */}
             <Tbody>
-              {/* TODO: Taskデータをここにマップして表示 */}
-              <Tr>
-                <Td width="40%" p={1} onClick={linkToShow}>
-                  Next.jsでTodoサイトを作成
+              {todos.map((todo) =>{
+
+                return(
+                  <Tr key={todo.Id}>
+                <Td width="40%" p={1} >
+                  <Link href={`/show/${todo.Id}`} style={{ cursor: "pointer" }} >
+                    {todo.Task}
+                  </Link>
                 </Td>
                 <Td width="12%" p={1}>
                   <Button p={2} bgColor="green.100" rounded="full" textAlign="center">
@@ -143,24 +160,28 @@ const Top = () => {
                 </Td>
                 <Td width="12%" p={1}>
                   <Select size="sm">
-                    <option>high</option>
-                    <option>middle</option>
-                    <option>low</option>
+                    <option value="High">high</option>
+                    <option value="Middle">middle</option>
+                    <option value="Low">low</option>
                   </Select>
                 </Td>
-                <Td width="12%" p={1}>
-                  yyyy-mm-dd hh:mm
+                <Td width="12%" p={2}>
+                  {todo.Create}
+                </Td>
+                <Td width="12%" p={2}>
+                  {todo.Update}
                 </Td>
                 <Td width="12%" p={1}>
-                  yyyy-mm-dd hh:mm
-                </Td>
-                <Td width="12%" p={1}>
-                  <></>
-                  <IconButton icon={<EditIcon />} size="xs" ml={4} onClick={linkToEdit} />
-                  <IconButton icon={<DeleteIcon />} size="xs" ml={4} onClick={DeleteTodo}/>
+                  <IconButton icon={<EditIcon />} size="xs" ml={4} onClick={() =>{linkToEdit(todo.Id)}} />
+                  <IconButton icon={<DeleteIcon />} size="xs" ml={4} onClick={() => {DeleteTodo(todo.Id)}} />
                 </Td>
               </Tr>
+                )
+              })}
+              
             </Tbody>
+            {/* Todoリスト */}
+
             {/* TODO: ページネーション機能挿入予定 */}
           </Table>
         </TableContainer>
